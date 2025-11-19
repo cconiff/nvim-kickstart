92572a11d9faf43cd3a86b5980acd4181aab8195
Caden Coniff <cconiff@gmail.com>
2025-11-19 12:59:54 -0700

    keeping old stuff for reference

---
+++ init_mini.lua
@@ -0,0 +1,105 @@
+-- add mini!
+local path_package = vim.fn.stdpath('data') .. '/site'
+local mini_path = path_package .. '/pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git', 'clone', '--filter=blob:none',
+    -- Uncomment next line to use 'stable' branch
+    -- '--branch', 'stable',
+    'https://github.com/nvim-mini/mini.nvim', mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+-- some sensible defaults
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.undofile = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- change leader map to space
+vim.g.mapleader = " "
+
+--tab behavior, default to 4 spaces
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 0 --changes to default tab
+
+--enable nerd font and color scheme!
+vim.g.have_nerd_font = true
+
+--should do color stuff!
+
+require("mini.basics").setup({
+  options = {
+    basic = true,
+    extra_ui = true,
+    win_borders = "bold",
+  },
+  mappings = {
+    basic = true,
+    windows = true,
+  },
+  autocommands = {
+    basic = true,
+    relnum_in_visual_mode = true,
+  },
+})
+
+require("mini.bracketed").setup()
+
+--clue to help with keys?
+local miniclue = require('mini.clue')
+miniclue.setup({
+  triggers = {
+    -- Leader triggers
+    { mode = 'n', keys = '<Leader>' },
+    { mode = 'x', keys = '<Leader>' },
+
+    -- `[` and `]` keys
+    { mode = 'n', keys = '[' },
+    { mode = 'n', keys = ']' },
+
+    -- Built-in completion
+    { mode = 'i', keys = '<C-x>' },
+
+    -- `g` key
+    { mode = 'n', keys = 'g' },
+    { mode = 'x', keys = 'g' },
+
+    -- Marks
+    { mode = 'n', keys = "'" },
+    { mode = 'n', keys = '`' },
+    { mode = 'x', keys = "'" },
+    { mode = 'x', keys = '`' },
+
+    -- Registers
+    { mode = 'n', keys = '"' },
+    { mode = 'x', keys = '"' },
+    { mode = 'i', keys = '<C-r>' },
+    { mode = 'c', keys = '<C-r>' },
+
+    -- Window commands
+    { mode = 'n', keys = '<C-w>' },
+
+    -- `z` key
+    { mode = 'n', keys = 'z' },
+    { mode = 'x', keys = 'z' },
+  },
+
+  clues = {
+    -- Enhance this by adding descriptions for <Leader> mapping groups
+    miniclue.gen_clues.square_brackets(),
+    miniclue.gen_clues.builtin_completion(),
+    miniclue.gen_clues.g(),
+    miniclue.gen_clues.marks(),
+    miniclue.gen_clues.registers(),
+    miniclue.gen_clues.windows(),
+    miniclue.gen_clues.z(),
+  },
+})
